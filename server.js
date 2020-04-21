f8ce2a964e9c40decdbb1a1b3d39b6d61bf47495
blucreative <40501667+fscodigital@users.noreply.github.com>
2020-04-21 06:22:36 -0400

    Ok World

--- server.js
+++ server.js
@@ -7,12 +7,18 @@ const app = express()
 const expressLayouts = require('express-ejs-layouts')
 
 const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/authors')
 
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
+
+// Body parser
+// app.use(express.json());
+app.use(express.urlencoded({ extended: false }))
+    .use(express.json());
 
 
 const mongoose = require('mongoose')
@@ -26,5 +32,6 @@ db.once('open', () => console.log('Connected to Mongoose'))
 
 
 app.use('/', indexRouter)
+app.use('/authors', authorRouter)
 
 app.listen(process.env.PORT || 3000)
